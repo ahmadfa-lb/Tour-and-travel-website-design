e740684ff727e15704dc1c4766af59abd761bdf6
ahmad farachi <103252037+ahmadfa-lb@users.noreply.github.com>
2024-11-24 15:23:07 +0200

    the footer section is  done < finish

--- js/script.js
+++ js/script.js
@@ -7,7 +7,6 @@ let menu = document.querySelector("#menu-bar");
 let navbar = document.querySelector(".navbar");
 let videoBtn = document.querySelectorAll(".vid-btn");
 
-
 window.onscroll = () => {
   searchBtn.classList.remove("fa-times");
   searchBar.classList.remove("active");
@@ -34,25 +33,24 @@ formClose.addEventListener("click", () => {
   loginForm.classList.remove("active");
 });
 
-videoBtn.forEach(btn => {
-  btn.addEventListener('click', ()=>{
-    document.querySelector('.controls .active').classList.remove('active');
-    btn.classList.add('active');
-    let src = btn.getAttribute('data-src');
-    document.querySelector('#video-slider').src = src
+videoBtn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    document.querySelector(".controls .active").classList.remove("active");
+    btn.classList.add("active");
+    let src = btn.getAttribute("data-src");
+    document.querySelector("#video-slider").src = src;
   });
 });
 
-
-var swiper = new Swiper('.review-slider', {
+var swiper = new Swiper(".review-slider", {
   spaceBetween: 20,
-  loop:true,
-  autoplay:{
-    delay:2500,
-    disableOnInteraction:false,
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
   },
-  breakpoints:{
-    640: { 
+  breakpoints: {
+    640: {
       slidesPerView: 1,
     },
     768: {
@@ -64,25 +62,25 @@ var swiper = new Swiper('.review-slider', {
   },
 });
 
-var swiper = new Swiper('.brand-slider', {
+var swiper = new Swiper(".brand-slider", {
   spaceBetween: 20,
-  loop:true,
-  autoplay:{
-    delay:2500,
-    disableOnInteraction:false,
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
   },
-  breakpoints:{
-    450:{
-      slidesPerView: 1,
-    },
-    768: { 
+  breakpoints: {
+    450: {
       slidesPerView: 2,
     },
-    991: {
+    768: {
       slidesPerView: 3,
     },
-    1200: {
+    991: {
       slidesPerView: 4,
+    },
+    1200: {
+      slidesPerView: 5,
     },
   },
 });
